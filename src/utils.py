3f61363138c212cfac477f773b596051707a73f2
camdenboren <boren.camden@gmail.com>
2024-08-30 14:53:09 -0500

    implemented same approach with create_cen_answer

--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,7 @@ def create_person_answer(topic, employees):
     return answer
 
 def create_cen_answer_helper(question, cen_answer):
-    """Format answer and increment cen_index"""
+    """Format answer based on question content"""
     if ("CEN" in question) and ("acronym" not in question) and ("abbreviation" not in question):
         answer = f"{cen_answer[0]}CEN{cen_answer[1]}"
     else:
@@ -14,16 +14,12 @@ def create_cen_answer_helper(question, cen_answer):
 
 def create_cen_answer(question, cen_answers, num_cen, cen_index):
     """Update CEN topics to be the relevant answer"""
-    if (cen_index in set(range(2))) or (cen_index in set(range(num_cen,num_cen+10))):
-        answer = create_cen_answer_helper(question, cen_answers["cen_0"])
-    elif ((cen_index in set(range(2,4))) and (num_cen >= 4)) or (cen_index in set(range(num_cen+10,num_cen+20))):
-        answer = create_cen_answer_helper(question, cen_answers["cen_1"])
-    elif ((cen_index in set(range(4,6))) and (num_cen >= 6)) or (cen_index in set(range(num_cen+20,num_cen+30))):
-        answer = create_cen_answer_helper(question, cen_answers["cen_2"])
-    elif ((cen_index in set(range(6,8))) and (num_cen >= 8)) or (cen_index in set(range(num_cen+30,num_cen+40))):
-        answer = create_cen_answer_helper(question, cen_answers["cen_3"])
-    elif ((cen_index in set(range(8,10))) and (num_cen >= 10)) or (cen_index in set(range(num_cen+40,num_cen+50))):
-        answer = create_cen_answer_helper(question, cen_answers["cen_4"])
+    answer = None
+    i = 0
+    while not answer:
+        if ((cen_index in set(range(i*2,(i+1)*2))) and (num_cen >= (i+1)*2)) or (cen_index in set(range(num_cen+(i*10),num_cen+((i+1)*10)))):
+            answer = create_cen_answer_helper(question, cen_answers[f"cen_{i}"])
+        i += 1
     cen_index += 1
     return answer, cen_index
 
@@ -32,7 +28,7 @@ def create_other_answer(answers, num, index):
     answer = None
     i = 0
     while not answer:
-        if ((index == i) or (index in set(range(num+(i*5), num+((i+1)*5))))):
+        if (index == i) or (index in set(range(num+(i*5), num+((i+1)*5)))):
             answer = answers[i]
         i += 1
     index += 1
