ad6ccebcabbe12d5342ad97b320e54576bfd43a4
camdenboren <boren.camden@gmail.com>
2024-10-31 14:15:53 -0500

    fmt, use python12

--- flake.nix
+++ flake.nix
@@ -7,42 +7,61 @@
     };
   };
 
-  outputs = { self, nixpkgs }: 
-  let
-    supportedSystems = [ "x86_64-linux" "aarch64-darwin" ];
-    forEachSupportedSystem = function: nixpkgs.lib.genAttrs supportedSystems (system: function rec {
-      pkgs = nixpkgs.legacyPackages.${system};
-      deps = with pkgs.python311Packages; [
-        ollama
+  outputs =
+    { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-darwin"
       ];
-    });
-  in {
-    devShells = forEachSupportedSystem ({ pkgs, deps }: {
-      default = pkgs.mkShell {
-        packages = with pkgs; [ 
-          bashInteractive 
-          python311 
-        ] ++ deps;
+      forEachSupportedSystem =
+        function:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          function rec {
+            pkgs = nixpkgs.legacyPackages.${system};
+            deps = with pkgs.python312Packages; [
+              ollama
+            ];
+          }
+        );
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs, deps }:
+        {
+          default = pkgs.mkShell {
+            packages =
+              with pkgs;
+              [
+                bashInteractive
+                python312
+              ]
+              ++ deps;
 
-        shellHook = ''
-          echo -e "\nchatbot-util Development Environment via Nix Flake\n"
-          python --version
-        '';
-      };
-    });
-    packages = forEachSupportedSystem ({ pkgs, deps }: {
-      default = pkgs.python311Packages.buildPythonApplication {
-        pname = "chatbot-util";
-        version = "0.1";
-        src = ./.;
+            shellHook = ''
+              echo -e "\nchatbot-util Development Environment via Nix Flake\n"
+              python --version
+            '';
+          };
+        }
+      );
+      packages = forEachSupportedSystem (
+        { pkgs, deps }:
+        {
+          default = pkgs.python312Packages.buildPythonApplication {
+            pname = "chatbot-util";
+            version = "0.1";
+            src = ./.;
 
-        propagatedBuildInputs = deps;
+            propagatedBuildInputs = deps;
 
-        meta = {
-          description = "Utility for generating similar FAQ's a la rag-fusion in a Dialogflow-ready, structured format.";
-          maintainers = [ "camdenboren" ];
-        };
-      };
-    });
-  };
+            meta = {
+              description = "Utility for generating similar FAQ's a la rag-fusion in a Dialogflow-ready, structured format.";
+              maintainers = [ "camdenboren" ];
+            };
+          };
+        }
+      );
+    };
 }
