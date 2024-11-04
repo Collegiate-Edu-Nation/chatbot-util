a0ced0c06baaadff10715f13f70d603f6a7277bc
camdenboren <boren.camden@gmail.com>
2024-11-04 16:03:52 -0600

    rm period in desc

--- flake.nix
+++ flake.nix
@@ -83,7 +83,7 @@
             propagatedBuildInputs = deps;
 
             meta = {
-              description = "Utility for generating similar FAQ's a la rag-fusion in a Dialogflow-ready, structured format.";
+              description = "Utility for generating similar FAQ's a la rag-fusion in a Dialogflow-ready, structured format";
               maintainers = [ "camdenboren" ];
             };
           };
