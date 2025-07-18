2574e2de94c573dbfe4938e1170d2f5cf253aa70
camdenboren <9UtEfABpSSrV3g.code@mailbox.org>
2025-07-18 11:09:58 -0500

    nix: introduce intermediate front-build pkg to enable starting frontend w/ `nix run .#front`

--- nix/package.nix
+++ nix/package.nix
@@ -16,16 +16,20 @@ let
     propagatedBuildInputs = deps.build;
   };
 
-  front = pkgs.buildNpmPackage {
+  front-build = pkgs.buildNpmPackage {
     inherit pname version;
     src = ../front/.;
     npmDepsHash = "sha256-UMyHZbT+pB5cmVX3wiQrug5zA+F2okHyK/vPwSl8kZI=";
   };
+
+  front = pkgs.writeShellScriptBin "chatbot-util" ''
+    ${pkgs.nodejs}/bin/npm run start --prefix ${front-build}/lib/node_modules/chatbot-util
+  '';
 in
 {
   inherit back front;
   default = pkgs.writeShellScriptBin "chatbot-util" ''
     ${back}/bin/chatbot-util &
-    ${pkgs.nodejs}/bin/npm run start --prefix ${front}/lib/node_modules/chatbot-util
+    ${front}/bin/chatbot-util
   '';
 }
