43fc971b73c10781f7a0b09d6975357912ab3c00
camdenboren <9UtEfABpSSrV3g.code@mailbox.org>
2025-07-28 11:01:53 -0500

    nix: rp pkgs -> prev in overlay

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -1,19 +1,18 @@
 # SPDX-FileCopyrightText: Collegiate Edu-Nation
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-{ pkgs }:
-
+final: prev:
 let
   # read script by name and patch bash shebang for nix users
   # see https://ertt.ca/nix/shell-scripts/
   writePatchedScript =
     name:
-    (pkgs.writeScriptBin name (builtins.readFile ../script/${name})).overrideAttrs (old: {
+    (prev.writeScriptBin name (builtins.readFile ../script/${name})).overrideAttrs (old: {
       buildCommand = "${old.buildCommand}\n patchShebangs $out";
     });
 in
-(final: prev: {
+{
   build = writePatchedScript "build";
   format = writePatchedScript "format";
   launch = writePatchedScript "launch";
-})
+}
