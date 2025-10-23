c120a006c0dfa4a376d0b48ea8778b147f7dc46c
camdenboren <9UtEfABpSSrV3g.code@mailbox.org>
2025-10-23 16:38:10 -0500

    docs: minor improvements to typedoc consistency

--- nix/shell.nix
+++ nix/shell.nix
@@ -12,17 +12,20 @@
       export DEV=true
       echo -e "\nchatbot-util DevShell via Nix Flake\n"
 
-      echo -e "┌───────────────────────────────────────────────┐"
-      echo -e "│                Useful Commands                │"
-      echo -e "├──────────┬────────────────────────────────────┤"
-      echo -e "│ Launch   │ $ launch                           │"
-      echo -e "│ Build    │ $ build                            │"
-      echo -e "│ Format   │ $ format                           │"
-      echo -e "│ Run      │ $ python -m src.chatbot_util       │"
-      echo -e "│ Test     │ $ python -m unittest               │"
-      echo -e "│ Coverage │ $ coverage run -m unittest         │"
-      echo -e "│ Docs     │ $ mkdocs {build, serve, gh-deploy} │"
-      echo -e "└──────────┴────────────────────────────────────┘"
+      echo -e "┌───────────────────────────────────────────────────┐"
+      echo -e "│                  Useful Commands                  │"
+      echo -e "├──────────────┬────────────────────────────────────┤"
+      echo -e "│ Launch       │ $ launch                           │"
+      echo -e "│ Build        │ $ build                            │"
+      echo -e "│ Docs         │ $ docs                             │"
+      echo -e "│ Format       │ $ format                           │"
+      echo -e "│ Run (Back)   │ $ python -m src.chatbot_util       │"
+      echo -e "│ Test (Back)  │ $ python -m unittest               │"
+      echo -e "│ Run (Front)  │ $ npm run dev                      │"
+      echo -e "│ Test (Front) │ $ npm test                         │"
+      echo -e "│ Coverage     │ $ coverage run -m unittest         │"
+      echo -e "│ mkDocs       │ $ mkdocs {build, serve, gh-deploy} │"
+      echo -e "└──────────────┴────────────────────────────────────┘"
     '';
   };
 }
