Collegiate-Edu-Nation/chatbot-util

commit 54d33fdece229c0144bca386985fea6bfdfb6a66

back: add ollama.url option to enable remote LLM servers
Also added typedoc to front's devDependencies since it was missing and I
didn't notice until I reinstalled my OS :)

back/src/chatbot_util/chain.py
@@ -3,9 +3,10 @@
 
 """Setup language model and output parser, then generate and append new questions"""
 
-import ollama
+from ollama import Client
 
 from chatbot_util import utils
+from src.chatbot_util import file_io
 
 
 class Progress:
@@ -79,7 +80,8 @@ def parse(response: str, phrases: list[list[str]]) -> list[str]:
 def invoke(prompt: str, phrases: list[list[str]]) -> list[str]:
     """Define chat model, then create the chain"""
     options = {"seed": 39}
-    response = ollama.generate(
+    host = file_io.read_config()["url"]
+    response = Client(host=host).generate(
         model="mistral",
         prompt=prompt,
         options=options,

back/src/chatbot_util/api.py
@@ -9,7 +9,7 @@ import time
 import fastapi
 import fastapi.middleware.cors
 import fastapi.staticfiles
-import ollama
+from ollama import Client
 
 from chatbot_util import __main__, chain, file_io, utils
 
@@ -39,7 +39,8 @@ def health(response: fastapi.Response) -> None:
     - `500` = ollama is not available
     """
     try:
-        ollama.show("mistral")
+        host = file_io.read_config()["url"]
+        Client(host=host).show("mistral")
     except Exception:
         response.status_code = fastapi.status.HTTP_500_INTERNAL_SERVER_ERROR
         utils.logger.error("You must install Ollama before using this utility.")
@@ -171,7 +172,9 @@ def config() -> dict[str, str]:
     - `faq` = CDN link to `FAQ - Enter Here.csv`
     - `other` = CDN link to `Other.txt`
     """
-    return file_io.read_config()
+    links = file_io.read_config()
+    links.pop("url")
+    return links
 
 
 # serve react frontend on root in production - DEV benefits from live reloads

back/src/chatbot_util/file_io.py
@@ -26,18 +26,22 @@ FILENAMES = {
 
 def read_config() -> dict[str, str]:
     """Read links from config file"""
-    links: dict[str, str] = {"faq": "", "other": ""}
+    cfg: dict[str, str] = {"url": "http://localhost:11434", "faq": "", "other": ""}
 
     try:
         with open(FILENAMES["config"], "rb") as f:
             config = tomllib.load(f)
 
+        ollama: dict[str, str] = config["ollama"]
         links: dict[str, str] = config["links"]
+        cfg = ollama | links
 
     except Exception:
-        utils.logger.warning("Failed to load config.toml. Defaulting to empty links")
+        utils.logger.warning(
+            "Failed to load config.toml. Defaulting to localhost and empty links"
+        )
 
-    return links
+    return cfg
 
 
 def create_file(f: fastapi.UploadFile) -> bool | None:

back/src/test/integration/test_chain_generate.py
@@ -3,11 +3,13 @@
 
 import unittest
 
-import ollama
 from mockito import (  # pyright: ignore [reportMissingTypeStubs]
+    ANY,  # pyright: ignore [reportUnknownVariableType]
+    mock,  # pyright: ignore [reportUnknownVariableType]
     unstub,  # pyright: ignore [reportUnknownVariableType]
     when,  # pyright: ignore [reportUnknownVariableType]
 )
+from ollama import Client
 
 from chatbot_util import chain
 
@@ -15,6 +17,7 @@ from chatbot_util import chain
 class TestChainGenerate(unittest.TestCase):
     def test_generate(self):
         # setup
+        client = mock(Client)
         phrases = [["abc", "ABC"]]
         options = {"seed": 39}
         prompt = chain.INSTRUCTION + "def"
@@ -23,14 +26,15 @@ class TestChainGenerate(unittest.TestCase):
         response2 = {"response": '"b"'}
         store = {"abc": ["def", "ghi"]}
 
-        # mock
-        when(ollama).generate(
+        # mocks
+        when(chain).Client(host=ANY(str)).thenReturn(client)  # pyright: ignore [reportUnknownMemberType]
+        when(client).generate(
             # pyright: ignore [reportUnknownMemberType]
             model="mistral",
             prompt=prompt,
             options=options,
         ).thenReturn(response)
-        when(ollama).generate(
+        when(client).generate(
             # pyright: ignore [reportUnknownMemberType]
             model="mistral",
             prompt=prompt2,

back/src/test/unit/test_file_io.py
@@ -11,13 +11,14 @@ from .. import utilities
 class TestFileIO(unittest.TestCase):
     def test_read_config(self):
         lines = [
-            "[links]\n",
+            '[ollama]\nurl = "http://localhost:11434"\n\n[links]\n',
             'faq = "abc"\n',
             'other = "def"\n',
         ]
 
         with utilities.TestFileContent(lines) as temp_file:
             expected_config = {
+                "url": "http://localhost:11434",
                 "faq": "abc",
                 "other": "def",
             }

nix/package.nix
@@ -12,7 +12,7 @@ let
     src = ../front/.;
 
     env.CYPRESS_INSTALL_BINARY = 0;
-    npmDepsHash = "sha256-kGSg6vTcoO0gp/pjfNTHRGqgmySZ7LxcdENOv79lPts=";
+    npmDepsHash = "sha256-PjA8nxFjMHBhVfnIsFMLyOROiBc8taA2gYg8lYnmF7w=";
     postInstall = ''
       cp -r dist/ $out/lib/node_modules/chatbot-util/
     '';

commit 65991a7f46021818d682bc863cf64ddc0a4c51a5

back: fix import error introduced in 54d33fd and tie logger to stdout
Fixes the service and keeps all darwin logs in /var/log/chatbot-util.log

back/src/chatbot_util/chain.py
@@ -5,8 +5,7 @@
 
 from ollama import Client
 
-from chatbot_util import utils
-from src.chatbot_util import file_io
+from chatbot_util import file_io, utils
 
 
 class Progress:

back/src/chatbot_util/utils.py
@@ -4,6 +4,7 @@
 """Utilities for logging and creating + cleaning answers based on file content"""
 
 import logging
+import sys
 from typing import TypedDict
 
 import coloredlogs  # pyright: ignore [reportMissingTypeStubs]
@@ -11,11 +12,16 @@ import coloredlogs  # pyright: ignore [reportMissingTypeStubs]
 # override logger's config in order to show non-uvicorn entries while attaching to it.
 # setting the formatting and colors make it match for info msgs
 format = "%(levelname)s:     chatbot_util    - %(message)s"
-logging.basicConfig(level=logging.INFO, format=format)
+logging.basicConfig(level=logging.INFO, format=format, stream=sys.stdout)
 logger = logging.getLogger("fastapi")
 colors = coloredlogs.DEFAULT_FIELD_STYLES
 colors["levelname"] = {"bold": False, "color": "green"}
-coloredlogs.install(level="INFO", fmt=format, field_styles=colors)  # pyright: ignore [reportUnknownMemberType]
+coloredlogs.install(  # pyright: ignore [reportUnknownMemberType]
+    level="INFO",
+    fmt=format,
+    field_styles=colors,
+    stream=sys.stdout,
+)
 
 # not interested in info logs for httpx or ollama
 logging.getLogger("httpx").setLevel(logging.WARNING)

nix/darwin-module.nix
@@ -37,6 +37,7 @@ in
         KeepAlive = true;
         ProcessType = "Background";
         RunAtLoad = true;
+        StandardOutPath = "/var/log/chatbot-util.log";
         Umask = 23;
         UserName = "root";
       };

back/src/test/unit/test_utils.py
@@ -1,12 +1,26 @@
 # SPDX-FileCopyrightText: Collegiate Edu-Nation
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+import logging
+import sys
 import unittest
+from typing import TextIO, cast
 
 from chatbot_util import utils
 
 
 class TestUtils(unittest.TestCase):
+    def test_logger_uses_stdout(self):
+        # some logs were previously being written to
+        # stderr, so this just makes sure we don't
+        # regress
+        streams: list[TextIO] = [
+            cast(TextIO, handler.stream)  # pyright: ignore [reportUnknownMemberType]
+            for handler in logging.getLogger().handlers
+            if isinstance(handler, logging.StreamHandler)
+        ]
+        self.assertEqual(streams, [sys.stdout])
+
     def test_create_person_answer(self):
         topic = "A Bcdef"
         employees = {"A Bcdef": ["G Hijk", "His"]}